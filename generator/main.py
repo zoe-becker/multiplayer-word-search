5350f18390d6a4f38e32256e2c01c6719394cd83
Ethan Reed <hyl6@txstate.edu>
2023-09-26 04:42:14 +0000

    endpoint for generator

---
+++ generator/main.py
@@ -0,0 +1,12 @@
+import os
+import sys
+from word_search_generator import WordSearch
+
+sys.path.insert(0, os.path.dirname(__file__))
+
+
+def application(environ, start_response):
+    start_response('200 OK', [('Content-Type', 'text/plain')])
+    puzzle = WordSearch("dog, cat, pig, horse, donkey, turtle, goat, sheep", "unicorn")
+    response = str(puzzle)
+    return [response.encode()]
