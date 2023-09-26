f12ffb9ae9fabc3eb1ceb086a3979eca9d88a770
Ethan Reed <hyl6@txstate.edu>
2023-09-26 04:48:04 +0000

    fix silly error

--- generator/main.py
+++ generator/main.py
@@ -7,6 +7,6 @@ sys.path.insert(0, os.path.dirname(__file__))
 
 def application(environ, start_response):
     start_response('200 OK', [('Content-Type', 'text/plain')])
-    puzzle = WordSearch("dog, cat, pig, horse, donkey, turtle, goat, sheep", "unicorn")
+    puzzle = WordSearch("dog, cat, pig, horse, donkey, turtle, goat, sheep, unicorn")
     response = str(puzzle)
     return [response.encode()]
