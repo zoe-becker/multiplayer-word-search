b8ba475fc0c6055c44381d8a36505be1e3f8c77e
Cruz Lopez <cml231@txstate.edu>
2023-09-28 00:26:21 -0500

    wrote pseudo code for SCRUM-57, added JSON

--- board/wordbank.js
+++ board/wordbank.js
@@ -1,4 +1,19 @@
 /**TODO
  * SCRUM-57
  * Display words on wordbank that aren't hardcoded
+ * 
+ *  When document is loaded do:
+ *      Fetch data from test json
+ *      
+ *      When data is fetched successfully:
+ *          parse the fetched data into json format
+ * 
+ *             when json parsing is successful:
+ *                  get the html element with ID "wordBankList" and store it as wordBankList
+ *                  
+ *                  For each word in the "words" list:
+ *                          create a new list item
+ *                           set the content of the list item to the current word.
+ *                           append the list item to the wordBankList element
+ * 
  */
