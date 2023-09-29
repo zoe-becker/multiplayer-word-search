dc56dae3160e7bea25c9832e45d00a3d754c43cb
Cruz Lopez <cml231@txstate.edu>
2023-09-28 22:25:57 -0500

    created wordsearch.js with pseudo code for grid

---
+++ board/wordsearch.js
@@ -0,0 +1,13 @@
+/*
+    create empty grid via 2d array 
+
+    let there be 13 rows
+    let there be 13 columns
+
+    create and array with rows var
+
+    for every row do:
+        for every column do:
+            array column row = 0
+
+*/
