27b0ec02cdee3ab20de3bce2b07a3637dc8ae1af
zoe becker 😹 <oiv13@txstate.edu>
2023-09-30 15:31:13 -0500

    js table for the board (also added letters)

--- board/wordbank.js
+++ board/wordbank.js
@@ -1,39 +1,57 @@
 /**TODO
  * SCRUM-57
  * Display words on wordbank that aren't hardcoded
- * 
+ *
  *  When document is loaded do:
  *      Fetch data from test json
- *      
+ *
  *      When data is fetched successfully:
  *          parse the fetched data into json format
- * 
+ *
  *             when json parsing is successful:
  *                  get the html element with ID "wordBankList" and store it as wordBankList
- *                  
+ *
  *                  For each word in the "words" list:
  *                          create a new list item
  *                           set the content of the list item to the current word.
  *                           append the list item to the wordBankList element
- * 
- * 
+ *
+ *
  * Add error handling later
  */
 
 // checks when page is loaded
-document.addEventListener('DOMContentLoaded', function(event) {
-    //When data is fetched successfully:
-    // parse the fetched data into json format
-    fetch('puzzle.json')
-        .then(response => response.json())
-        .then(data=>{
-            // get the html element with ID "wordBankList" and store it as wordBankList
-            const wordBankList = document.getElementById("wordBankList");
+document.addEventListener("DOMContentLoaded", function (event) {
+  //When data is fetched successfully:
+  // parse the fetched data into json format
+  fetch("puzzle.json")
+    .then((response) => response.json())
+    .then((data) => {
+      // get the html element with ID "wordBankList" and store it as wordBankList
+      const wordBankList = document.getElementById("wordBankList");
 
-            data.words.forEach(word => {
-                let listItem = document.createElement("li"); //  create a new list item
-                listItem.textContent = word;                // set contnet of the list item to the current word
-                wordBankList.appendChild(listItem);         //  append the list item to the word banklist element
-            })
-        })
-})
+      data.words.forEach((word) => {
+        let listItem = document.createElement("li"); //  create a new list item
+        listItem.textContent = word; // set contnet of the list item to the current word
+        wordBankList.appendChild(listItem); //  append the list item to the word banklist element
+      });
+
+      // draw the word search
+      renderWordSearch(data.puzzle);
+    });
+});
+
+function renderWordSearch(puzzle) {
+  let table = document.createElement("table");
+  const container = document.getElementById("wordsearch");
+  container.appendChild(table); // Drew the main table node on the document
+
+  puzzle.forEach(function (row) {
+    let tr = table.insertRow(); //Create a new row
+
+    row.forEach(function (column) {
+      let td = tr.insertCell();
+      td.innerText = column; // Take string from placeholder variable and append it to <tr> node
+    });
+  });
+}
