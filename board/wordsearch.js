73bb818813c5833ea8b1d3ead764b5a81b95b426
Cruz Lopez <cml231@txstate.edu>
2023-09-30 15:36:09 -0500

    updated json parsing code

--- board/wordsearch.js
+++ board/wordsearch.js
@@ -4,7 +4,7 @@ When the document is loaded:
     when the data is received:
     convert data to a json object
     when conversion is done:
-        get html element with id "grid" and store it in a variable "wordGrid"
+        get html element with id "grid" and store it in a variable "grid"
 
         For each "row" in data.puzzle:
             create a new tbale row and store it in tRow
@@ -23,5 +23,22 @@ document.addEventListener('DOMContentLoaded', function(event) {
         .then(data => {
             const grid = document.getElementById("grid"); // changed "wordGrid" to "grid"
             // add logic here to add data to grid
+
+            data.puzzle.forEach(row=>{
+                
+
+                let tRow = document.createElement('tr');
+
+                // store the value of cell in tCell and append it to the row
+                row.forEach(cell =>{
+                    let tCell = document.createElement('td');
+                    tCell.innerText = cell;
+                    tRow.appendChild(tCell);
+                });
+
+                // append the row (with the cell now added) to the grid
+                grid.appendChild(tRow);
+            });
+
         })
 });
