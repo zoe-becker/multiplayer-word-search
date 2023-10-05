bb085de935fd8288a8938360edbc4c44caba6aff
Melanie Garza <mmg252@txstate.edu>
2023-10-05 11:16:04 -0500

    Homepage base
    made the base of the homepage using html and css and added a picture to background

--- index.php
+++ index.php
@@ -1,9 +1,20 @@
+
 <html>
     <head>
-
+    <link rel="stylesheet" href="style.css">
     </head>
     <body>
-        <h1>I LOVE WORD SEARCH</h1>
-        <h2> pipeline test </h2>
+        <div class="gif">
+        <nav> 
+            <ul> 
+                <li><a href="">Home</a></li>
+                <li><a href="">About</a></li>
+                <li><a href="">Credits</a></li>
+            </ul>
+        </nav>
+        <h1> Welcome to WordSearch </h1>
+            <button type="button"> Lets Play! </button>
+        </div>
+
     </body>
 </html>
