7c6db82834dbf27d3b16021e8262734cdce08029
Melanie Garza <mmg252@txstate.edu>
2023-10-06 11:44:04 -0500

    Added css to all buttons & changed logo

--- index.php
+++ index.php
@@ -8,7 +8,7 @@
     <body>
         <div class="gif">
         <nav> 
-            <img src="gorns.png" class="logo">
+            <img src="magnifying-glass.png" class="logo">
             <ul> 
                 <li><a href="">Home</a></li>
                 <li><a href="">About</a></li>
@@ -16,8 +16,8 @@
             </ul>
         </nav>
         <h1> WordSearch! </h1>
-            <button onclick="requestBoard()" type="button"> <span> </span>Lets Play!</button>
-            <button onclick="location.href='/word-search-generator/leaderboard'" type="button">Leaderboard</button>
+            <button onclick="requestBoard()" type="button"> <span> </span>Play</button>
+            <button onclick="location.href='/word-search-generator/leaderboard'" type="button"><span> </span> Leaderboard</button>
             <!-- <button onclick="requestBoard()">Play</button> -->
         </div>
     </body>
