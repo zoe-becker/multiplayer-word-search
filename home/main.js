8ad51bdc3a6ddd87c66dddc59d3b4e84297ec768
Ethan Reed <hyl6@txstate.edu>
2023-10-23 13:48:38 -0500

    add a lock to prevent multiple instance creation

--- home/main.js
+++ home/main.js
@@ -1,19 +1,25 @@
-function requestBoard() {
-    // start request for new board
-    let request = new XMLHttpRequest();
+let requestingLobbyLock = false;
 
-    // handle response
-    request.onreadystatechange = function() {
-        if (request.readyState == 4) {
-            if (request.status == 200) {
-                location.href = request.responseText;
-            } else {
-                console.log("AJAX Error:" + request.responseText);
+function createLobby() {
+    if (requestingLobbyLock == false) {
+        // start request for new board
+        requestingLobbyLock = true;
+        let request = new XMLHttpRequest();
+
+        // handle response
+        request.onreadystatechange = function() {
+            if (request.readyState == 4) {
+                if (request.status == 200) {
+                    location.href = request.responseText;
+                } else {
+                    console.log("AJAX Error:" + request.responseText);
+                    requestingLobbyLock = false; // allow user to try again
+                }
             }
         }
-    }
 
-    // send request
-    request.open("POST", "play.php");
-    request.send();
+        // send request
+        request.open("POST", "createLobby.php");
+        request.send();
+    }
 }
