8bc1ac1c71d1327a2521c73543983084b7c2a44d
Cameron C <cac615@txstate.edu>
2023-10-24 21:43:51 -0500

    Starterflexboxes

--- lobby/template/index.php
+++ lobby/template/index.php
@@ -1,2 +1,19 @@
 <!DOCTYPE html>
-<!-- lobby html here -->
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Lobby</title>
+    <link rel="stylesheet" href="lobby.css">
+    <script src="lobby.js"></script>
+    <body>
+            <div class="lobby-container">
+                <div class="lobby-title">Flex Box 1</div>
+                <div class="lobby-playerlistandsettings">
+                    <div class="host-settings-buttons"></div>
+                    <div class ="player-list"></div>
+                </div>
+                <div class="lobby-sharecode">Flex Box 3</div>
+            </div>
+        </div>
+    </body>
+</html>
