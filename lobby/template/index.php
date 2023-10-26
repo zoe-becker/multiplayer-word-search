7479bb8eeebb55869e3316d94d7e8a91ca8312de
Cameron C <cac615@txstate.edu>
2023-10-26 11:44:18 -0500

    checkpoint

--- lobby/template/index.php
+++ lobby/template/index.php
@@ -12,7 +12,12 @@
                 </div>
                 <div class="lobby-playerlistandsettings">
                     <div class="host-settings-buttons"></div>
-                    <div class ="player-list"></div>
+                    <div class ="player-list-box">
+                        <div class ="player-list-title">
+                            <div class= "player-list-title-text">Player List</div>
+                        </div>
+                        <div class ="player-list"> </div>
+                    </div>
                 </div>
                 <div class="lobby-sharecode">Flex Box 3</div>
             </div>
