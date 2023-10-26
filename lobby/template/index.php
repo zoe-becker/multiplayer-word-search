cf1f94a1727ce0d9f7c1073e9bb07ad6240368a7
Cameron C <cac615@txstate.edu>
2023-10-26 15:00:04 -0500

    commit before js hell

--- lobby/template/index.php
+++ lobby/template/index.php
@@ -1,4 +1,4 @@
-<!DOCTYPE html>
+
 <html lang="en">
 <head>
     <meta charset="UTF-8">
@@ -11,7 +11,12 @@
                     <div class="lobby-title-info">Wordsearch!</div>
                 </div>
                 <div class="lobby-playerlistandsettings">
-                    <div class="host-settings-buttons"></div>
+                    <div class="host-settings-buttons">
+                            <div class="word-search-grid" id="word-search-grid">
+                                 <!-- The word search grid will be populated here -->
+                                 <!-- START (4,5)->(4,9) to be green, THEMES (1,7)->(1,12) to be red, and HOW TO PLAY (8,3)->(8,13) to be yellow.--> 
+                            </div>
+                    </div>
                     <div class ="player-list-box">
                         <div class ="player-list-title">
                             <div class= "player-list-title-text">Player List</div>
@@ -20,7 +25,10 @@
                     </div>
                 </div>
                 <div class="lobby-sharecode">Flex Box 3</div>
+                <form id="add-player-form">
+                    <input type="text" id="player-name" placeholder="Enter player name">
+                    <button type="button" onclick="simulateAddPlayer()">Add Player</button>
+                </form>
             </div>
-        </div>
     </body>
 </html>
--- lobby/template/lobby.js
+++ lobby/template/lobby.js
@@ -1,1 +1,40 @@
 //lobby js
+function addPlayer(name) {
+    var playerList = document.getElementById('player-list');
+    var playerBox = document.createElement('div');
+    playerBox.classList.add('player-box');
+    playerBox.textContent = name;
+    playerList.appendChild(playerBox);
+}
+
+function simulateAddPlayer() {
+    var playerName = document.getElementById('player-name').value;
+    if (playerName !== '') {
+        addPlayer(playerName);
+    }
+}
+fetch('lobbywordsearchgrid.json')
+    .then(response => response.json())
+    .then(data => {
+        const wordSearchGrid = data.wordSearchGrid;
+        const gridContainer = document.getElementById('word-search-grid');
+
+        // Populate the word search grid
+        wordSearchGrid.forEach(row => {
+            const rowElement = document.createElement('div');
+            rowElement.classList.add('row');
+
+            row.forEach(cell => {
+                const cellElement = document.createElement('div');
+                cellElement.classList.add('cell');
+                cellElement.textContent = cell;
+                rowElement.appendChild(cellElement);
+            });
+
+            gridContainer.appendChild(rowElement);
+        });
+    });
+    console.log("JavaScript file is linked successfully.");
+
+   
+   
