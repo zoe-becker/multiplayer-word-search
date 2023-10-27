619234d17a15e5dedeb636c97a887a6d71557d61
Cameron C <cac615@txstate.edu>
2023-10-27 10:24:57 -0500

    before adding class to current data-special-cell

--- lobby/template/lobby.js
+++ lobby/template/lobby.js
@@ -16,6 +16,35 @@ function simulateAddPlayer() {
     }
 }
     console.log("JavaScript file is linked successfully.");
+// Get all elements with the data-special-cell attribute
+const specialCells = document.querySelectorAll('[data-special-cell]');
+// Log the size of the specialCells array
+console.log("Size of specialCells array:", specialCells.length);
 
+// Add event listeners for click
+specialCells.forEach(cell => {
+    cell.addEventListener('click', function() {
+        // Handle click event based on the type of data-special-cell
+        const cellType = cell.getAttribute('data-special-cell');
+        switch(cellType) {
+            case 'themes':
+                // Handle themes click
+                console.log('Themes clicked:', cell.textContent);
+                break;
+            case 'start':
+                // Handle types click
+                console.log('start clicked:', cell.textContent);
+                break;
+            case 'how-to-win!':
+                // Handle types click
+                console.log('htw clicked:', cell.textContent);
+                break;
+            default:
+                // Handle default case
+                console.log('Default clicked:', cell.textContent);
+                // Add your default logic here
+        }
+    });
+});
    
    
