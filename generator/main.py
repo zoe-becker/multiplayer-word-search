ae8928ef9706ced1e421322bc6e9078abcf4c950
Melanie Garza <mmg252@txstate.edu>
2023-10-26 23:57:52 -0500

    adjusted main.py so that it calls json file

--- generator/main.py
+++ generator/main.py
@@ -1,11 +1,12 @@
 import os
 import sys
 import random
-from wordDictionary import words
+import json
 from word_search_generator import WordSearch
 
 sys.path.insert(0, os.path.dirname(__file__))
 
+THEME = 'nicki.json'
 
 def application(environ, start_response):
     start_response('200 OK', [('Content-Type', 'text/plain')])
@@ -16,6 +17,8 @@ def application(environ, start_response):
 
 
 def random_words():
+    file_path = f'c:/cs3398 downloads/htdocs/word-search-generator/generator/themes/{THEME}'
+    words = load_json(file_path)
     wordBank= []
     word = random.sample(range(0, len(words["words"])), 9)
     random.sample(range(0, len(words["words"])), 9)
@@ -25,3 +28,16 @@ def random_words():
         
     result = ', '.join(map(str, wordBank))
     return result
+
+
+def load_json(file_path):
+    try:
+        with open(file_path, 'r', encoding='utf-8') as f:
+            data = json.load(f)
+        return data
+    except json.JSONDecodeError as e:
+        print(f"Error decoding JSON: {e}")
+    except FileNotFoundError:
+        print("The file was not found.")
+    except Exception as e:
+        print(f"An error occurred: {e}")
