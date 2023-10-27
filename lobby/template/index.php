6f4697dff46ed8539a59b34b00dfc1bfe3102152
Cameron C <cac615@txstate.edu>
2023-10-27 12:19:48 -0500

    Theme updates

--- lobby/template/index.php
+++ lobby/template/index.php
@@ -34,24 +34,24 @@
                                     <div class="cell">X</div><br>
                                 </div>
                                 <div class="row">
-                                    <div data-special-cell="settings">S</div>
+                                    <div class="cell" data-special-cell="settings">S</div>
                                     <div class="cell">E</div>
                                     <div class="cell">K</div>
                                     <div class="cell">E</div>
                                     <div class="cell">A</div>
                                     <div class="cell">R</div>
                                     <div class="cell">S</div>
-                                    <div data-special-cell="themes">T</div>
-                                    <div data-special-cell="themes">H</div>
-                                    <div data-special-cell="themes">E</div>
-                                    <div data-special-cell="themes">M</div>
-                                    <div data-special-cell="themes">E</div>
-                                    <div data-special-cell="themes">S</div>
+                                    <div class="cell" data-special-cell="themes">T</div>
+                                    <div class="cell" data-special-cell="themes">H</div>
+                                    <div class="cell" data-special-cell="themes">E</div>
+                                    <div class="cell" data-special-cell="themes">M</div>
+                                    <div class="cell" data-special-cell="themes">E</div>
+                                    <div class="cell" data-special-cell="themes">S</div>
                                     <div class="cell">D</div>
                                     <div class="cell">P</div>
                                 </div>
                                 <div class="row">
-                                    <div data-special-cell="settings">E</div>
+                                    <div class="cell" data-special-cell="settings">E</div>
                                     <div class="cell">H</div>
                                     <div class="cell">E</div>
                                     <div class="cell">H</div>
@@ -68,7 +68,7 @@
                                     <div class="cell">O</div>
                                 </div>
                                 <div class="row">
-                                    <div data-special-cell="settings">T</div>
+                                    <div class="cell" data-special-cell="settings">T</div>
                                     <div class="cell">O</div>
                                     <div class="cell">T</div>
                                     <div class="cell">D</div>
@@ -85,16 +85,16 @@
                                     <div class="cell">A</div>
                                 </div>
                                 <div class="row">
-                                    <div data-special-cell="settings">T</div>
+                                    <div class="cell" data-special-cell="settings">T</div>
                                     <div class="cell">E</div>
                                     <div class="cell">T</div>
                                     <div class="cell">T</div>
                                     <div class="cell">I</div>
-                                    <div data-special-cell="start">S</div>
-                                    <div data-special-cell="start">T</div>
-                                    <div data-special-cell="start">A</div>
-                                    <div data-special-cell="start">R</div>
-                                    <div data-special-cell="start">T</div>
+                                    <div class="cell" data-special-cell="start">S</div>
+                                    <div class="cell" data-special-cell="start">T</div>
+                                    <div class="cell" data-special-cell="start">A</div>
+                                    <div class="cell" data-special-cell="start">R</div>
+                                    <div class="cell" data-special-cell="start">T</div>
                                     <div class="cell">P</div>
                                     <div class="cell">I</div>
                                     <div class="cell">C</div>
@@ -102,7 +102,7 @@
                                     <div class="cell">F</div>
                                 </div>
                                 <div class="row">
-                                    <div data-special-cell="settings">I</div>
+                                    <div class="cell" data-special-cell="settings">I</div>
                                     <div class="cell">X</div>
                                     <div class="cell">V</div>
                                     <div class="cell">B</div>
@@ -119,7 +119,7 @@
                                     <div class="cell">L</div>
                                 </div>
                                 <div class="row">
-                                    <div data-special-cell="settings">N</div>
+                                    <div class="cell" data-special-cell="settings">N</div>
                                     <div class="cell">D</div>
                                     <div class="cell">S</div>
                                     <div class="cell">A</div>
@@ -136,7 +136,7 @@
                                     <div class="cell">C</div>
                                 </div>
                                 <div class="row">
-                                    <div data-special-cell="settings">G</div>
+                                    <div class="cell" data-special-cell="settings">G</div>
                                     <div class="cell">H</div>
                                     <div class="cell">J</div>
                                     <div class="cell">E</div>
@@ -153,20 +153,20 @@
                                     <div class="cell">L</div>
                                 </div>
                                 <div class="row">
-                                    <div data-special-cell="settings">S</div>
+                                    <div class="cell" data-special-cell="settings">S</div>
                                     <div class="cell">L</div>
                                     <div class="cell">Q</div>
-                                    <div data-special-cell="how-to-win!">H</div>
-                                    <div data-special-cell="how-to-win!">O</div>
-                                    <div data-special-cell="how-to-win!">W</div>
-                                    <div data-special-cell="how-to-win!"></div>
-                                    <div data-special-cell="how-to-win!">T</div>
-                                    <div data-special-cell="how-to-win!">O</div>
-                                    <div data-special-cell="how-to-win!"></div>
-                                    <div data-special-cell="how-to-win!">W</div>
-                                    <div data-special-cell="how-to-win!">I</div>
-                                    <div data-special-cell="how-to-win!">N</div>
-                                    <div data-special-cell="how-to-win!">!</div>
+                                    <div class="cell" data-special-cell="how-to-win!">H</div>
+                                    <div class="cell" data-special-cell="how-to-win!">O</div>
+                                    <div class="cell" data-special-cell="how-to-win!">W</div>
+                                    <div class="cell" data-special-cell="how-to-win!"></div>
+                                    <div class="cell" data-special-cell="how-to-win!">T</div>
+                                    <div class="cell" data-special-cell="how-to-win!">O</div>
+                                    <div class="cell" data-special-cell="how-to-win!"></div>
+                                    <div class="cell" data-special-cell="how-to-win!">W</div>
+                                    <div class="cell" data-special-cell="how-to-win!">I</div>
+                                    <div class="cell" data-special-cell="how-to-win!">N</div>
+                                    <div class="cell" data-special-cell="how-to-win!">!</div>
                                     <div class="cell">E</div>
                                 </div>
                                 <div class="row">
@@ -214,12 +214,13 @@
             </div>
             <div id ="player-list-box">
                     <div id ="player-list-title">
-                         <div id= "player-list-title-text">Player List</div>
+                         <div id= "player-list-title-text">Players</div>
                      </div>
                     <div id="player-list-container"> </div>
                     <div id="selected-theme-container">
                         <!-- somehow put selected theme here lol -->
-                        <div p="selected-theme-text">Current Theme</div>    
+                        <div class="selected-theme-text">Current Theme:</div>
+                        <div class="selected-theme-text">Holidays</div>        
                     </div>
             </div>
         </div>
--- lobby/template/lobby.js
+++ lobby/template/lobby.js
@@ -16,35 +16,34 @@ function simulateAddPlayer() {
     }
 }
     console.log("JavaScript file is linked successfully.");
-// Get all elements with the data-special-cell attribute
-const specialCells = document.querySelectorAll('[data-special-cell]');
-// Log the size of the specialCells array
-console.log("Size of specialCells array:", specialCells.length);
 
-// Add event listeners for click
-specialCells.forEach(cell => {
-    cell.addEventListener('click', function() {
-        // Handle click event based on the type of data-special-cell
-        const cellType = cell.getAttribute('data-special-cell');
-        switch(cellType) {
-            case 'themes':
-                // Handle themes click
-                console.log('Themes clicked:', cell.textContent);
-                break;
-            case 'start':
-                // Handle types click
-                console.log('start clicked:', cell.textContent);
-                break;
-            case 'how-to-win!':
-                // Handle types click
-                console.log('htw clicked:', cell.textContent);
-                break;
-            default:
-                // Handle default case
-                console.log('Default clicked:', cell.textContent);
-                // Add your default logic here
-        }
-    });
-});
-   
+    document.addEventListener('DOMContentLoaded', (event) => {
+    // Get all elements with the data-special-cell attribute
+    const specialCells = document.querySelectorAll('[data-special-cell]');
+    // Log the size of the specialCells array
+    console.log("Size of specialCells array:", specialCells.length);
+
+    // Add event listeners for click
+    specialCells.forEach(cell => {
+        cell.addEventListener('mouseover', function() {
+            const cellType = cell.getAttribute('data-special-cell');
+            specialCells.forEach(specialCell => {
+                if (specialCell.getAttribute('data-special-cell') === cellType) {
+                    specialCell.classList.add('brighten');
+                }
+            });
+        });
+
+        cell.addEventListener('mouseout', function() {
+            specialCells.forEach(specialCell => {
+                specialCell.classList.remove('brighten');
+            });
+        });
+        cell.addEventListener('click', function() {
+            // Handle click event based on the type of data-special-cell
+                const cellType = cell.getAttribute('data-special-cell');
+                console.log(`Clicked on cell with attribute ${cellType}:`, cell.textContent);
+            });
+        });
+    }); 
    
