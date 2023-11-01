59e7e50752d8c2f79b349c7ebb5ba4d4f6508135
Melanie Garza <mmg252@txstate.edu>
2023-10-31 23:12:20 -0500

    delete exisiting code from setName

--- lobby/setTheme.php
+++ lobby/setTheme.php
@@ -1,13 +1,4 @@
 <?php
-    /* This function is responsible for adding a new player to a particular lobby
-       The first person to set their name in the lobby becomes the host
-       Accepted methods: POST only
-       Required headers:
-          1. name: name of new player
-          2. lobby: lobby id (including the prefix)
-
-        Return: setNameResponse object
-    */
 
     require "../utilities/fileSyncronization.php";
     require "../utilities/requestValidation.php";
@@ -44,7 +35,7 @@
         exit;
     }
 
-
+// function used to verify if theme is in list of possible themes
     function verifyTheme($theme){
         $possibleThemes = getThemeNames();
         if(!in_array($theme, $possibleThemes)){
@@ -57,39 +48,7 @@
 
     function main() {
         global $LOBBY_DATAFILE_NAME;
-        validatePOST(array("lobby", "name"), true); // validate request
         
-        $requestedName = $_SERVER["HTTP_NAME"];
-        $lobbyID = $_SERVER["HTTP_LOBBY"];
-
-        validateLobby($lobbyID, true); // validate lobby exists
-
-        $lobbyDataPath = "$lobbyID/$LOBBY_DATAFILE_NAME";
-        $lobbyStream = flock_acquireEX($lobbyDataPath); // acquire lock on file since we may write to it
-        $lobbyData = json_decode(fread($lobbyStream, filesize($lobbyDataPath)), true);
-
-        // validate name is valid and game hasn't started
-        validateRequest($lobbyData, $requestedName);
-
-        $player = addPlayer($lobbyData, $requestedName);
-        $isHost = $player["isHost"];
-        // rewind stream so new data can be written and write new player
-        rewind($lobbyStream); 
-        fwrite($lobbyStream, json_encode($lobbyData));
-        flock_release($lobbyStream); // release lock
-        
-        $response = array(
-            "accessToken" => $player["accessToken"],
-            "isHost" => $isHost
-        );
-
-        if ($isHost) {
-            $response["themes"] = getThemeNames();
-        }
-
-        http_response_code(200);
-        echo json_encode($response);
     }
-
     main();
 ?>
