e88bc0ef01d748b383635e7d85da20d88363bc93
Melanie Garza <mmg252@txstate.edu>
2023-11-15 12:36:04 -0600

    removed credits page and removed home button from landing page

--- home/index.php
+++ home/index.php
@@ -11,9 +11,7 @@
         <nav> 
             <img src="magnifying-glass.png" class="logo">
             <ul> 
-                <li><a href="">Home</a></li>
                 <li><a href="">About Us</a></li>
-                <li><a href="">Credits</a></li>
             </ul>
         </nav>
         <h1> WordSearch! </h1>
