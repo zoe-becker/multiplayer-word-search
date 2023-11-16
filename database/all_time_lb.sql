156d90a71cd71d598b6d668e5bd6584add409627
Cruz Lopez <cml231@txstate.edu>
2023-11-16 09:49:47 -0600

    updated default leaderboard with test values

--- database/all_time_lb.sql
+++ database/all_time_lb.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Nov 16, 2023 at 03:55 AM
+-- Generation Time: Nov 16, 2023 at 04:46 PM
 -- Server version: 10.4.28-MariaDB
 -- PHP Version: 8.2.4
 
@@ -45,7 +45,32 @@ INSERT INTO `all_time_lb` (`time_stamp`, `Player`, `Score`, `theme`) VALUES
 ('2023-11-03 17:38:47', 'Zoe', 800, 'animals'),
 ('2023-11-03 17:38:47', 'Mel', 600, 'animals'),
 ('2023-11-03 17:38:47', 'Cruz', 400, 'animals'),
-('2023-11-03 17:38:47', 'Ethan', 200, 'animals');
+('2023-11-03 17:38:47', 'Ethan', 200, 'animals'),
+('2023-11-03 17:38:47', 'Cruz', 1200, 'Christmas'),
+('2023-11-03 17:38:47', 'Ethan', 800, 'Christmas'),
+('2023-11-03 17:38:47', 'Mel', 750, 'Christmas'),
+('2023-11-03 17:38:47', 'Cameron', 550, 'Christmas'),
+('2023-11-03 17:38:47', 'Zoe', 400, 'Christmas'),
+('2023-11-03 17:38:47', 'Ethan', 9999, 'Nicki'),
+('2023-11-03 17:38:47', 'Mel', 4500, 'Nicki'),
+('2023-11-03 17:38:47', 'Cameron', 1600, 'Nicki'),
+('2023-11-03 17:38:47', 'Zoe', 800, 'Nicki'),
+('2023-11-03 17:38:47', 'Cruz', 350, 'Nicki'),
+('2023-11-03 17:38:47', 'Zoe', 3600, 'Halloween'),
+('2023-11-03 17:38:47', 'Mel', 800, 'Halloween'),
+('2023-11-03 17:38:47', 'Cameron', 1450, 'Halloween'),
+('2023-11-03 17:38:47', 'Ethan', 750, 'Halloween'),
+('2023-11-03 17:38:47', 'Cruz', 200, 'Halloween'),
+('2023-11-03 17:38:47', 'Mel', 2000, 'thanksgiving'),
+('2023-11-03 17:38:47', 'Zoe', 1995, 'thanksgiving'),
+('2023-11-03 17:38:47', 'Ethan', 1005, 'thanksgiving'),
+('2023-11-03 17:38:47', 'Cameron', 1989, 'thanksgiving'),
+('2023-11-03 17:38:47', 'Cameron', 500, 'thanksgiving'),
+('2023-11-03 17:38:47', 'Cameron', 900, 'valentine'),
+('2023-11-03 17:38:47', 'Cameron', 850, 'valentine'),
+('2023-11-03 17:38:47', 'Cameron', 1005, 'valentine'),
+('2023-11-03 17:38:47', 'Cameron', 1337, 'valentine'),
+('2023-11-03 17:38:47', 'Cameron', 260, 'valentine');
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
