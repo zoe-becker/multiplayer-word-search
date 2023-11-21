7e3c3cc65f0c742601ce359bc7d6ba78dad7317b
Cruz Lopez <cml231@txstate.edu>
2023-11-21 09:27:26 -0600

    fixed highlighter logic, updated font and colors

--- leaderboard/index.php
+++ leaderboard/index.php
@@ -6,7 +6,7 @@
     <title>Leaderboard</title>
     <link rel="stylesheet" type="text/css" href="styles.css">
     <script src="https://unpkg.com/htmx.org@1.9.8" integrity="sha384-rgjA7mptc2ETQqXoYC3/zJvkU7K/aP44Y+z7xQuJiVnB/422P/Ak+F/AqFR7E4Wr" crossorigin="anonymous"></script>
-
+    <script src="https://unpkg.com/hyperscript.org@0.9.12"></script>
 </head>
 <body>
     <div class="container">
@@ -15,7 +15,7 @@
         </div>
         <!-- HTMX Tabs for Leaderboards -->
         <div id="leaderboard-tabs" hx-target="#leaderboard-content" role="tablist" _="on htmx:afterOnLoad set @aria-selected of <[aria-selected=true]/> to false tell the target take .selected set @aria-selected to true">
-            <button role="tab" aria-controls="leaderboard-content" aria-selected="true" hx-get="leaderboard.php?theme=all-time" class="selected">All-Time</button>
+            <button role="tab" aria-controls="leaderboard-content" aria-selected="true" hx-get="leaderboard.php?theme=all-time" class=selected>All-Time</button>
             <button role="tab" aria-controls="leaderboard-content" aria-selected="false" hx-get="leaderboard.php?theme=animals">Animals</button>
             <button role="tab" aria-controls="leaderboard-content" aria-selected="false" hx-get="leaderboard.php?theme=christmas">Christmas</button>
             <button role="tab" aria-controls="leaderboard-content" aria-selected="false" hx-get="leaderboard.php?theme=halloween">Halloween</button>
@@ -23,8 +23,10 @@
             <button role="tab" aria-controls="leaderboard-content" aria-selected="false" hx-get="leaderboard.php?theme=valentine">Valentine's</button>
             <button role="tab" aria-controls="leaderboard-content" aria-selected="false" hx-get="leaderboard.php?theme=thanksgiving">Thanksgiving</button>
             <!-- Add more tabs as needed -->
+            
         </div>
-        
+        <div class="tabsShadow"></div>
+            <div class="glider"></div>
         <div class="scores-container">
             <div id="leaderboard-content" role="tabpanel" hx-get="leaderboard.php?theme=all-time" hx-trigger="load">
         <!-- Leaderboard content will be loaded here -->
