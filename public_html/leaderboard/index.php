95c4f4354d04a3856f310b764ea134058e41c805
zoe becker 😹 <oiv13@txstate.edu>
2023-11-25 21:57:03 -0600

    added redirect to home button for leaderboard
    house icon is not working need to look into that later

--- public_html/leaderboard/index.php
+++ public_html/leaderboard/index.php
@@ -5,10 +5,17 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Leaderboard</title>
     <link rel="stylesheet" type="text/css" href="styles.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <script src="https://unpkg.com/htmx.org@1.9.8" integrity="sha384-rgjA7mptc2ETQqXoYC3/zJvkU7K/aP44Y+z7xQuJiVnB/422P/Ak+F/AqFR7E4Wr" crossorigin="anonymous"></script>
     <script src="https://unpkg.com/hyperscript.org@0.9.12"></script>
 </head>
 <body>
+    <nav> 
+        <ul> 
+            <li><a href="../home"><i class="fa fa-home"></i>Home</a></li>
+        </ul>
+    </nav>
+
     <div class="container">
         <div class="leaderboard">
             <h1>Leaderboards</h1>
