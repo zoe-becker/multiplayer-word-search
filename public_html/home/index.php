febc96b33a5a734231b5bb21fcfe0b53366370d3
Melanie Garza <mmg252@txstate.edu>
2023-11-27 21:57:40 -0600

    minor css changes

--- public_html/home/index.php
+++ public_html/home/index.php
@@ -17,31 +17,13 @@
         </nav>
        
         <h1> WordSearch! </h1>
-        <div class="midheaders">
-            <h2> Solo Play </h2> 
-            <h2> Multiplayer </h2>
-        </div>
        
         <div class="buttons">
             <div class="time-attack-buttons">
                 <div class="timeAttack"> 
+                <h2> Solo Play </h2> 
                     <button onclick="toggleScreen('time-attack-user-screen','show')" type="button"> <span> </span>Time Attack</button>
-                    <!-- <button  onClick="toggleScreen('time-attack-screen','show')" class="info-button" type="button"> <span> </span> i </button> -->
                 </div>
-
-                <!-- <div id="time-attack-screen" class="hidden">
-                    <div id="time-attack-content">
-                        <p1>WHAT IS TIME ATTACK?</p1>
-                        <br></br>
-                        <p>In Time Attack, players are challenged to find as many</p>
-                        <p>words as possible within a fixed time limit. The score</p>
-                        <p>multiplier deceases as time passes instead of increasing.<p>
-                        <div class="close-button">
-                            <button onClick="toggleScreen('time-attack-screen','hide')" id="close-button"><span> </span>Close</button>
-                        </div>
-                        
-                    </div> 
-                </div> -->
 
                 <div id="time-attack-user-screen" class="hidden">
                     <div id="time-attack-user-content">
@@ -62,6 +44,7 @@
             <div> 
             
             <div class="create-game"> 
+            <h2> Multiplayer </h2>
                 <button onclick="createLobby()" type="button"> <span> </span>Create Lobby</button>
             </div>
           
